03b603edadd89b4f3f5ec31a54199b8085b37d33
muhammedzihadinfo <muhammedzihad.info@gmail.com>
2026-09-21 00:41:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-console.log("🤖 Messenger Group Bot is starting...");
+const http = require("http");
 
-console.log("Node.js is working!");
+const PORT = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+
+  res.end("🤖 Messenger Group Bot is running!");
+});
+
+server.listen(PORT, () => {
+  console.log(`🤖 Bot server is running on port ${PORT}`);
+});
